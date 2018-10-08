fd5c45f13c0b60bdaaa9ee3bda0b5c7aa4c6e054
Vladimir Deryuzhenko <317110@gmail.com>
2018-10-08 13:43:19 -0400

    +

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+def countdown(int)
+  while int > 0 do
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+#write your code here
+def countdown_with_sleep(int)
+  while int > 0 do
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
